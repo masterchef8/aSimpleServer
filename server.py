0fe3ae6024d9959d23cedc20a245da59994c2570
pgiraude <philippe.giraudeau@inria.fr>
2017-12-27 20:39:42 +0100

    Handle GET request + modify Json creation

--- server.py
+++ server.py
@@ -1,31 +1,38 @@
 import socket
-import CreateListOfFiles
+
 import CreateJson
-import os
+import netifaces as ni
 
-cwd = os.getcwd()
 
-createList = CreateListOfFiles.CreateListofFile()
-createJson = CreateJson.CreateJson(createList.getList())
+def parse(string) -> str:
+    s = string[6:string.find(" HTTP")]
 
+    if len(s) > 1:
+        string = s[1:]
+        return string
+    else:
+        return "nothing"
+
+
+createJson = CreateJson.CreateJson()
 HOST, PORT = '', 8888
+HOST_LOCAL = '127.0.0.1'  # ni.ifaddresses('lo0').get(2)[0].get('addr')
+HOST_NETWORK = ni.ifaddresses('en0').get(2)[0].get('addr')
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
+listen_socket.bind((HOST_LOCAL, PORT))
 listen_socket.listen(1)
 
+# print('Serving HTTP on port %s ...' % PORT)
 
-print ('Serving HTTP on port %s ...' % PORT)
+print("address : " + HOST_LOCAL + ":" + str(PORT))
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    print(request)
+    r = parse(str(request))
 
-    http_response = """\
-HTTP/1.1 200 OK
+    http_response = createJson.createDict
 
-Hello, World!
-"""
-    client_connection.sendall(http_response)
+    client_connection.send(http_response.encode())
     client_connection.close()
