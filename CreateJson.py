79b9cf9012a7b44a1c966e6eb23ed129c7fc3464
pgiraude <philippe.giraudeau@inria.fr>
2018-01-04 16:30:04 +0100

    final commit

--- CreateJson.py
+++ CreateJson.py
@@ -1,20 +1,16 @@
 import json
 import os
 
-
 class CreateJson:
     """docstring for CreateJson """
 
-    def __init__(self, path=os.getcwd()):
-        self.path = path
-        if self.path == os.getcwd():
+    def __init__(self, path=os.getcwd(), net_adress="193.50.110.74:8888"):
+        self.path = os.getcwd() + '/data'
+        self.net_address = net_adress
+        self.list = os.listdir(self.path)
+        self.net_address_data = "http://"+net_adress+"/"+"data"
 
-            self.path = os.getcwd()+'/data'
-            self.list = os.listdir(self.path)
-        else:
-            self.list = os.listdir(self.path)
-
-    @property
+        self.createDict()
     def createDict(self):
 
         data = {}
@@ -27,61 +23,24 @@ class CreateJson:
 
             if file_extension == ".txt":
 
-                data['txt'][self.list[i]] = {"name": filename, "extension": file_extension,
-                                      "path": str(self.path + '/' + filename), "type": "text"}
+                data['txt'][self.list[i]] = {"name": self.list[i], "extension": file_extension,
+                                             "path": str(self.net_address_data + '/' + self.list[i]), "type": "text"}
 
             elif file_extension == ".png" or file_extension == ".jpg" or file_extension == "jpeg" or \
                     file_extension == ".gif":
 
-                data['img'][self.list[i]] = {"name": filename, "extension": file_extension,
-                                      "path": str(self.path + '/' + filename), "type": "picture"}
+                data['img'][self.list[i]] = {"name": self.list[i], "extension": file_extension,
+                                             "path": str(self.net_address_data + '/' + self.list[i]), "type": "picture"}
 
             elif file_extension == ".mp4" or file_extension == ".mov" or file_extension == ".avi":
-                data['video'][self.list[i]] = {"name": filename, "extension": file_extension,
-                                      "path": str(self.path + '/' + filename), "type": "video"}
+                data['video'][self.list[i]] = {"name": self.list[i], "extension": file_extension,
+                                               "path": str(self.net_address_data + '/' + self.list[i]), "type": "video"}
 
         json_file = json.dumps(data)
         print(json_file)
-        return json_file
+        obj = open('data.txt', 'wb+')
+        j = json_file.encode()
+        obj.write(j)
+        obj.close()
+        # return json_file
 
-
-"""
-JSON{
-	http://developer.rhino3d.com/guides/rhinopython/python-dictionary-database/
-	datastore = { "office": {
-    "medical": [
-      { "room-number": 100,
-        "use": "reception",
-        "sq-ft": 50,
-        "price": 75
-      },
-      { "room-number": 101,
-        "use": "waiting",
-        "sq-ft": 250,
-        "price": 75
-      },
-      { "room-number": 102,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 103,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 104,
-        "use": "office",
-        "sq-ft": 150,
-        "price": 100
-      }
-    ],
-    "parking": {
-      "location": "premium",
-      "style": "covered",
-      "price": 750
-    }
-  }
-}
-}
-"""
--- server.py
+++ server.py
@@ -1,32 +1,33 @@
 import socket
 
+import sys
+
 import CreateJson
 import netifaces as ni
+import http.server
 
-
-def parse(string) -> str:
-    s = string[6:string.find(" HTTP")]
-
-    if len(s) > 1:
-        string = s[1:]
-        return string
-    else:
-        return "nothing"
-
-
-createJson = CreateJson.CreateJson()
 HOST, PORT = '', 8888
 HOST_LOCAL = '127.0.0.1'  # ni.ifaddresses('lo0').get(2)[0].get('addr')
 HOST_NETWORK = ni.ifaddresses('en0').get(2)[0].get('addr')
 
+
+if __name__ == "__main__":
+    createJson = CreateJson.CreateJson(net_adress=HOST_NETWORK+":"+str(PORT))
+
+    handlerClass = http.server.BaseHTTPRequestHandler
+
+    http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, bind=HOST_NETWORK, port=PORT)
+
+"""
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST_LOCAL, PORT))
+listen_socket.bind((HOST_NETWORK, PORT))
 listen_socket.listen(1)
+"""
+"""-----------------------------------------------------------------------"""
 
-# print('Serving HTTP on port %s ...' % PORT)
-
-print("address : " + HOST_LOCAL + ":" + str(PORT))
+"""
+print("address : " + HOST_NETWORK + ":" + str(PORT))
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
@@ -36,3 +37,4 @@ while True:
 
     client_connection.send(http_response.encode())
     client_connection.close()
+"""
